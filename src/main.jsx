e6a1e79da866c042c1309c4e1b150367c35e22c5
Tegaaumu <tegaumurhurhu@gmail.com>
2026-08-01 13:05:16 +0100

    Extracting JSX Into a New Component

--- src/main.jsx
+++ src/main.jsx
@@ -135,10 +135,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour}/>
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -149,6 +146,15 @@ function Footer() {
   );
   //older version
   // return React.createElement('footer', null, "we're currently open!")
+}
+
+function Order(props) {
+    return(
+        <div className="order">
+          <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+    )
 }
 
 function Pizza(props) {
