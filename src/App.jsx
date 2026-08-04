ebd181f27b487806591f0130f08416d455906a2c
Tegaaumu <tegaumurhurhu@gmail.com>
2026-08-04 10:20:22 +0100

    {travel list} Building a Form and Handling Submissions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
 export default function App() {
-  return(
+  return (
     <div className="a">
       <Logo />
       <Form />
@@ -9,23 +15,56 @@ export default function App() {
   );
 }
 
-function Logo(){
-  return <h1> 🌴 Far Away</h1>
+function Logo() {
+  return <h1> 🌴 Far Away</h1>;
 }
-function Form(){
-  return(
-    <div className="add-form">
+function Form() {
+
+  function handleSumit(e){
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSumit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..."/>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackingList(){
-  return <div className="list">List</div>
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
-function Stats(){
-  return(
+function Stats() {
+  return (
     <footer>
       <em>💼 You have X items on your list, and you already packed X (X%)</em>
     </footer>
-  )
+  );
 }
