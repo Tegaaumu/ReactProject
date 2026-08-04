5daa703b7c7b5c4daa2af5b8640762877e33a2d6
Tegaaumu <tegaumurhurhu@gmail.com>
2026-08-04 11:28:06 +0100

    flashCard change state

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./index.css";
+import "./style.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import FlashCards from "./FlashCards";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <FlashCards />
   </React.StrictMode>,
 );
