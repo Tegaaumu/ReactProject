0d32f6bc889d6ba86732483d7abd561fe0ee4618
Tegaaumu <tegaumurhurhu@gmail.com>
2026-08-04 09:42:53 +0100

    {travel list} setting up function

---
+++ src/Section 2/App.jsx
@@ -0,0 +1,87 @@
+// we place message out sode App function so that its will not be rendered everytime the App function is called.
+
+import { useState } from "react";
+
+// note this two thing are different.
+//onMouseEnter={alert("test")}
+//onMouseEnter={() => alert("test")}
+//useState(1) is an hook. anything that start with `use` is a hook. it can only be called at top(first) imediately after the function.
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  // const step = 2;
+  const [step, setStep] = useState(1);
+  // below is wrong
+  // const [test, setTest] = useState({ name: "Jonas" });
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    //old
+    // if (step > 1) setStep(step - 1);
+    //updated/lastest
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    // the below is wrong
+    // step = step + 1;
+    //Good practise
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+    //Bad practise
+    // test.name = "Tega";
+    //Good practice
+    // setTest({ name: "Fred" });
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
